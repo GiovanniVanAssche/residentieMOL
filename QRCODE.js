86585a144eec136b84a9c44df4b996c1f87c4f9f
GiovanniVanAssche <34142722+GiovanniVanAssche@users.noreply.github.com>
2019-05-04 08:57:56 +0200

    Update QRCODE.js

--- QRCODE.js
+++ QRCODE.js
@@ -1,1 +1,29 @@
-
+var QRCODE = {code:["290807019493"]}; 
+ 
+ 
+ function check(form){  
+           
+     for (var i = 0; i < QRCODE.code.length ; i++)  { 
+         
+       if ((form.QRCODE.value == QRCODE.code[i]))  { 
+       
+        valid = true;
+        
+           break;  
+      } 
+      else{
+        
+       
+      if (form.QRCODE.value !== QRCODE.code[i]){
+           alert("De opgegeven code klopt niet");          
+           break;
+           }
+       }
+    }   
+       
+    
+    if (valid)   {    
+      window.location.assign('final.html');    
+      return false;   
+    }           
+}
